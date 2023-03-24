b2d4188e1f277ec00d3518fedbed89a67b384820
Ethmane Didi <ousman.didibela@gmail.com>
2023-03-24 11:51:02 +0100

    Finished Author Rutes

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 // Routes
 const indexRouter = require('./routes/index');
@@ -16,8 +17,9 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: false}))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
@@ -29,8 +31,7 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
-app.use("/authors", authorRouter)
-app.use("/books", bookRouter)
-
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 4000);
